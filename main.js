465faaa6652d773bf23de467781433c04482ec82
RyanDBarnett <conner2468@gmail.com>
2019-02-11 18:18:21 -0700

    Pseudo code onLoad and createIdeaCard functions

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
 // Query select the idea title input box and assign it to var titleInput
 // Query select the idea body input box and assign it to var bodyInput
 // Query select the save button and assign it to var saveBtn
+// Query select the card container section and assign it to var cardContainer
 
 // Add a click event listener to saveBtn with a function called saveIdea
+// Add a click event listener to the cardContainer with a function called ?
+
+// Create an empty ideas array to hold all the new idea instances
+
+// onLoad function
+  // Retrieve JSON ideas array from local storage
+  // Parse the ideas array and assign it to the global ideas array variable
+  // For every idea in ideas array loop
+    // create an idea card for each idea
 
 // saveIdea function
   // Take the value of the titleInput and assign it to var title
   // Take the value of the bodyInput and assign it to var body
   // Create a new idea instance using the title and body and assign to var idea
   // Save the idea to storage with Idea.saveToStorage(idea)
-  // Clear the titleInput value
-  // Clear the body Input value
+  // Call clearInputValue function with title as argument
+  // Call clearInputValue function with body as argument
+
+
+// clearInputValue(value) function
+  // Set given value to empty string
+
+
+// createIdeaCard(ideaObj) function
+  // Targeting cardContainer use add Inner HTML to add a card using a template literal
+  // Interpolate ideaObj.title and ideaObj.body into appropriate places in template literal
+
