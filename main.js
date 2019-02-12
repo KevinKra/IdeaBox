48ba1784edcfaa95483f197e75db40e4ef703aa2
RyanDBarnett <conner2468@gmail.com>
2019-02-11 17:29:48 -0700

    Create basic pseudo code for main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+// Query select the idea title input box and assign it to var titleInput
+// Query select the idea body input box and assign it to var bodyInput
+// Query select the save button and assign it to var saveBtn
+
+// Add a click event listener to saveBtn with a function called saveIdea
+
+// saveIdea function
+  // Take the value of the titleInput and assign it to var title
+  // Take the value of the bodyInput and assign it to var body
+  // Create a new idea instance using the title and body and assign to var idea
+  // Save the idea to storage with Idea.saveToStorage(idea)
+  // Clear the titleInput value
+  // Clear the body Input value
