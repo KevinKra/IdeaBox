08a52f6954dbd053b9e54603ba70c2151eb26de7
RyanDBarnett <conner2468@gmail.com>
2019-02-12 19:28:38 -0700

    Remove pseudo code

--- main.js
+++ main.js
@@ -3,11 +3,6 @@ var bodyInput = document.querySelector('#body-textarea');
 var savBtn = document.querySelector('.btn-save-idea');
 var cardContainer = document.querySelector('.output-content');
 
-// Query select the idea title input box and assign it to var titleInput
-// Query select the idea body input box and assign it to var bodyInput
-// Query select the save button and assign it to var saveBtn
-// Query select the card container section and assign it to var cardContainer
-
 var stringifiedIdeas = localStorage.getItem('ideas') || '[]';
 ideas = JSON.parse(stringifiedIdeas);
 var ideaCounter = 0;
@@ -16,22 +11,12 @@ var ideaCounter = 0;
 savBtn.addEventListener('click', saveIdea);
 cardContainer.addEventListener('click', buttonChecker);
 onLoad();
-// Add a click event listener to saveBtn with a function called saveIdea
-// Add a click event listener to the cardContainer with a function called buttonChecker
-
-// Create an empty ideas array to hold all the new idea instances
-
 
 function onLoad() {
   for (var i = 0; i < ideas.length; i++) {
     createIdeaCard(ideas[i]);
   }
 }
-// onLoad function
-  // Retrieve JSON ideas array from local storage
-  // Parse the ideas array and assign it to the global ideas array variable
-  // For every idea in ideas array loop
-    // create an idea card for each idea
 
 function saveIdea() {
   var title = titleInput.value;
@@ -42,23 +27,10 @@ function saveIdea() {
   clearInputValues(title, body); 
 }
 
-
-// saveIdea function
-  // Take the value of the titleInput and assign it to var title
-  // Take the value of the bodyInput and assign it to var body
-  // Create a new idea instance using the title and body and assign to var idea
-  // Save the idea to storage with Idea.saveToStorage(idea)
-  // Call clearInputValue function with title as argument
-  // Call clearInputValue function with body as argument
-
-
 function clearInputValues(title, body) {
   title.value = "";
   body.value = "";
 }
-// clearInputValue(value) function
-  // Set given value to empty string
-
 
 function createIdeaCard(ideaObj) {
   var card = document.createElement('article');
@@ -81,9 +53,6 @@ function createIdeaCard(ideaObj) {
   card.innerHTML = html;
   cardContainer.appendChild(card);
 }
-// createIdeaCard(ideaObj) function
-  // Targeting cardContainer use add Inner HTML to add a card using a template literal
-  // Interpolate ideaObj.title and ideaObj.body into appropriate places in template literal
 
 function incrementCounter() {
   ideaCounter++;
@@ -107,11 +76,3 @@ function buttonChecker(e){
       // updateQuality();
   }
 }
-// buttonChecker(e) function
-  // If the event target's id is equal to the up button's id then...
-    // updateQuality()
-  // If the event target's id is equal to the down button's id then...
-    // updateQuality()
-  // If the event target's id is equal to the delete button's id then...
-    // Delete card from local storage using deleteFromStorage(id)
-    // Use parent element (maybe multiple times) on the event target to target the card and remove it
